14c30f006e087bdae78d1c04162d9286238ce559
Victor Hideo Oka <oka.victor97@gmail.com>
2020-04-11 17:06:43 -0300

    setup for view code usage

---
+++ viperdex/viperdex/PokemonListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonListViewController.swift
+//  viperdex
+//
+//  Created by Victor Oka on 11/04/20.
+//  Copyright © 2020 Victor Oka. All rights reserved.
+//
+
+import UIKit
+
+class PokemonListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
